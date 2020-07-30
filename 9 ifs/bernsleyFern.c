7599f8b74de94892e7d896a3b371ead8ca981dd6
choijean <choijean@pdx.edu>
2020-07-30 07:07:27 +0000

    finish bernsley fern

--- 9 ifs/bernsleyFern.c
+++ 9 ifs/bernsleyFern.c
@@ -34,22 +34,72 @@ int main()
 	G_clear () ;
 	G_rgb (0.3, 0.5, 0.7) ; //light blue
 
+  double mx, my, ml, ma ;
+  double gx, gy, gl, ga ;
+  double rx, ry, rl, ra ;
+  double yx, yy, yl, ya ;
+  double px, py, pl, pa ;
+  double angle ;
+  double sf ;
+
+  mx = 26 ;     my = 99 ;
+  gx = 26 ;     gy = 83 ;     //green
+  rx = -20.5 ;  ry = 27.5 ;   //red
+  yx = 24 ;     yy = 31 ;     //yellow
+  px = 0 ;      py = 16 ;     //purple
+
+  ml = sqrt(pow(mx,2) + pow(my,2)) ;
+  gl = sqrt(pow(gx,2) + pow(gy,2)) ;
+  rl = sqrt(pow(rx,2) + pow(ry,2)) ;
+  yl = sqrt(pow(yx,2) + pow(yy,2)) ;
+  pl = sqrt(pow(px,2) + pow(py,2)) ;
+
+  ma = (180/M_PI) * atan2(my,mx) ;
+  ga = (180/M_PI) * atan2(gy,gx) ;
+  ra = (180/M_PI) * atan2(ry,rx) ;
+  ya = (180/M_PI) * atan2(yy,-yx) ;
+  pa = (180/M_PI) * atan2(py,px) ;
+
+
   int j = 0;
   double r;
   while (j < 1000000) {
     r = drand48() ; //random double s.t. 0 <= r < 1
 
-    if (r < 0.8) {
-      G_rgb(0,1,0) ;
-      sf = gl/ml ;
-      angle = ga - ma ;
+    if (r < 0.8) {          // green 
+      //G_rgb(0,1,0) ;
+      sf = gl/ml ;          // 0.849740
+      angle = ga - ma ;     // ~ -2.7
       translate(-0.5, 0) ;
       rotate(angle) ;
       scale(sf, sf);
       translate(0, 16.0/100) ;
       translate(0.5, 0) ;
-    } else if (r < 2.0/3.0) {
-    } else {
+    } else if (r < 0.9) {   // red
+      //G_rgb(1,0,0);
+      sf = rl/ml;           // 0.335102
+      angle = ra - ma;      //  51
+      translate(-0.5, 0);
+      rotate(angle);
+      scale(sf, sf);
+      translate(0, 16.0/100);
+      translate(0.5,0);
+    } else if (r < 0.98) {  // yellow
+      //G_rgb(1,1,0);
+      sf = yl/ml;           // 0.383017 
+      angle = ya - ma;      // 52
+      translate(-0.5, 0);
+      rotate(angle);
+      scale(-sf, sf);
+      translate(0, 4.0/100);
+      translate(0.5,0);
+    } else {                // purple
+      //G_rgb(1,0,1);
+      sf = pl/ml;           // 0.156315
+      angle = pl - ma;      // -59
+      translate(-0.5, 0);
+      scale(0.0, sf);
+      translate(0.5,0);
     }
     G_point(swidth*x[0], sheight*y[0]);
     j++;
--- 9 ifs/jean.c
+++ 9 ifs/jean.c
@@ -237,170 +237,61 @@ int main()
       case 64 :
         translate(0/s, 6/s);
         break;
-      case 65 :
-        translate(0/s, 7/s);
-        break;
-      case 66 :
-        translate(0/s, 8/s);
-        break;
-      case 67 :
-        translate(0/s, 9/s);
-        break;
-      case 68 :
-        translate(0/s, 10/s);
-        break;
-      case 69 :
-        translate(0/s, 11/s);
-        break;
-      case 70 :
-        translate(0/s, 12/s);
-        break;
-      case 71 :
-        translate(0/s, 13/s);
-        break;
-      case 72 :
-        translate(0/s, 14/s);
-        break;
-      case 73 :
-        translate(0/s, 15/s);
-        break;
-      case 74 :
-        translate(0/s, 16/s);
-        break;
-      case 75 :
-        translate(0/s, 17/s);
-        break;
-      case 76 :
-        translate(0/s, 18/s);
-        break;
-      case 77 :                     // J negative space
-        translate(1/s, 15/s);
-        break;
-      case 78 :
-        translate(2/s, 15/s);
-        break;
-      case 79 :
-        translate(3/s, 15/s);
-        break;
-      case 80 :
-        translate(4/s, 15/s);
-        break;
-      case 81 :
-        translate(5/s, 15/s);
-        break;
-      case 82 :
-        translate(5/s, 14/s);
-        break;
-      case 83 :                    // E negative space top
-        translate(14/s, 16/s);
-        break;
-      case 84 :
-        translate(15/s, 16/s);
-        break;
-      case 85 :
-        translate(16/s, 16/s);
-        break;
-      case 86 :
-        translate(17/s, 16/s);
-        break;
-      case 87 :
-        translate(18/s, 16/s);
-        break;
-      case 88 :                   // E negative space bottom
-        translate(14/s, 13/s);
-        break;
-      case 89 :
-        translate(15/s, 13/s);
-        break;
-      case 90 :
-        translate(16/s, 13/s);
-        break;
-      case 91 :
-        translate(17/s, 13/s);
-        break;
-      case 92 :
-        translate(18/s, 13/s);
-        break;
-      case 93 :                   // N negative space top
-        translate(16/s, 8/s);
-        break;
-      case 94 :
-        translate(16/s, 7/s);
-        break;
-      case 95 :
-        translate(16/s, 6/s);
-        break;
-      case 96 :
-        translate(16/s, 5/s);
-        break;
-      case 97 :                 // N negative space bottom
-        translate(13/s, 4/s);
-        break;
-      case 98 :
-        translate(13/s, 3/s);
-        break;
-      case 99 :
-        translate(13/s, 2/s);
-        break;
-      case 100 :
-        translate(13/s, 1/s);
-        break;
-      case 101 :                // A negative space
-        translate(4/s, 5/s);
-        break;
-      case 102 :
-        translate(4/s, 1/s);
-        break;
-      case 103 :                  // vert space between letters
-        translate(9/s, 17/s);
-        break;
-      case 104 :
-        translate(9/s, 15/s);
-        break;
-      case 105 :
-        translate(9/s, 13/s);
-        break;
-      case 106 :
-        translate(9/s, 11/s);
-        break;
-      case 107 :
-        translate(9/s, 9/s);
-        break;
-      case 108 :
-        translate(9/s, 7/s);
-        break;
-      case 109 :
-        translate(9/s, 5/s);
-        break;
-      case 110 :
-        translate(9/s, 3/s);
-        break;
-      case 111 :
-        translate(9/s, 1/s);
-        break;
-      case 112 :                  // horizontal space between letters
-        translate(1/s, 9/s);
-        break;
-      case 113 :
-        translate(3/s, 9/s);
-        break;
-      case 114 :
-        translate(5/s, 9/s);
-        break;
-      case 115 :
-        translate(7/s, 9/s);
-        break;
-      case 116 :
-        translate(11/s, 9/s);
-        break;
-      case 117 :
-        translate(13/s, 9/s);
-        break;
-      case 118 :
-        translate(15/s, 9/s);
-        break;
-      default :
-        translate(17/s, 9/s);
+      case 65 : translate(0/s, 7/s); break;
+      case 66 : translate(0/s, 8/s); break;
+      case 67 : translate(0/s, 9/s); break;
+      case 68 : translate(0/s, 10/s); break;
+      case 69 : translate(0/s, 11/s); break;
+      case 70 : translate(0/s, 12/s); break;
+      case 71 : translate(0/s, 13/s); break;
+      case 72 : translate(0/s, 14/s); break;
+      case 73 : translate(0/s, 15/s); break;
+      case 74 : translate(0/s, 16/s); break;
+      case 75 : translate(0/s, 17/s); break;
+      case 76 : translate(0/s, 18/s); break;
+      case 77 : translate(1/s, 15/s); break; // J negative space
+      case 78 : translate(2/s, 15/s); break;
+      case 79 : translate(3/s, 15/s); break;
+      case 80 : translate(4/s, 15/s); break;
+      case 81 : translate(5/s, 15/s); break;
+      case 82 : translate(5/s, 14/s); break;
+      case 83 : translate(14/s, 16/s); break; // E negative space top
+      case 84 : translate(15/s, 16/s); break;
+      case 85 : translate(16/s, 16/s); break;
+      case 86 : translate(17/s, 16/s); break;
+      case 87 : translate(18/s, 16/s); break;
+      case 88 : translate(14/s, 13/s); break; // E negative space bottom
+      case 89 : translate(15/s, 13/s); break;
+      case 90 : translate(16/s, 13/s); break;
+      case 91 : translate(17/s, 13/s); break;
+      case 92 : translate(18/s, 13/s); break;
+      case 93 : translate(16/s, 8/s); break; // N negative space top
+      case 94 : translate(16/s, 7/s); break;
+      case 95 : translate(16/s, 6/s); break;
+      case 96 : translate(16/s, 5/s); break;
+      case 97 : translate(13/s, 4/s); break; // N negative space bottom
+      case 98 : translate(13/s, 3/s); break;
+      case 99 : translate(13/s, 2/s); break;
+      case 100 : translate(13/s, 1/s); break;
+      case 101 : translate(4/s, 5/s); break; // A negative space
+      case 102 : translate(4/s, 1/s); break;
+      case 103 : translate(9/s, 17/s); break; // vert space between letters
+      case 104 : translate(9/s, 15/s); break;
+      case 105 : translate(9/s, 13/s); break;
+      case 106 : translate(9/s, 11/s); break;
+      case 107 : translate(9/s, 9/s); break;
+      case 108 : translate(9/s, 7/s); break;
+      case 109 : translate(9/s, 5/s); break;
+      case 110 : translate(9/s, 3/s); break;
+      case 111 : translate(9/s, 1/s); break;
+      case 112 : translate(1/s, 9/s); break; // horizontal space between letters
+      case 113 : translate(3/s, 9/s); break;
+      case 114 : translate(5/s, 9/s); break;
+      case 115 : translate(7/s, 9/s); break;
+      case 116 : translate(11/s, 9/s); break;
+      case 117 : translate(13/s, 9/s); break;
+      case 118 : translate(15/s, 9/s); break;
+      default : translate(17/s, 9/s);
     }
 
     G_point(swidth*x[0], sheight*y[0]);
